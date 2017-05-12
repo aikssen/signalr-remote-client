35319c565c31f323581d0eda5d9411425f65b9eb
Ever Cifuentes <ever.cifuentes@globant.com>
2017-05-11 20:19:16 -0500

    add complex object to the server

--- C#/SignalRChat/ChatHub.cs
+++ C#/SignalRChat/ChatHub.cs
@@ -5,10 +5,13 @@ namespace SignalRChat
 {
     public class ChatHub : Hub
     {
+        public string UserName { get; set; }
+        public string Message { get; set; }
+
         public void Send(string name, string message)
         {
             // Call the broadcastMessage method to update clients.
-            Clients.All.broadcastMessage(name, message);
+            Clients.All.broadcastMessage(new ChatHub() { UserName = name, Message = message });
             //Clients.ToString();
         }
     }
